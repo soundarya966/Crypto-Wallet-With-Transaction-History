c329143542bf8fa2a599d07b8afd14637c998dae
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 23:02:51 +0530

    Update WalletService.java

--- src/WalletService.java
+++ src/WalletService.java
@@ -1,1 +1,50 @@
+import java.util.ArrayList;
 
+public class WalletService {
+
+    ArrayList<Transaction> transactions =
+            new ArrayList<>();
+
+    // Send cryptocurrency
+    public void sendMoney(
+            Wallet sender,
+            Wallet receiver,
+            double amount
+    ) {
+
+        if (sender.getBalance() >= amount) {
+
+            sender.deductBalance(amount);
+
+            receiver.addBalance(amount);
+
+            Transaction transaction =
+                    new Transaction(
+                            sender.getOwnerName(),
+                            receiver.getOwnerName(),
+                            amount
+                    );
+
+            transactions.add(transaction);
+
+            System.out.println(
+                    "\nTransaction Successful!"
+            );
+
+        } else {
+
+            System.out.println(
+                    "\nInsufficient Balance!"
+            );
+        }
+    }
+
+    // Display transactions
+    public void displayTransactions() {
+
+        for (Transaction transaction : transactions) {
+
+            transaction.displayTransaction();
+        }
+    }
+}
